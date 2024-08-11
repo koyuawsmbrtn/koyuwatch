6b6c063ec9dc2ecdbd41bb161e9cb903021bfe77
koyu <me@koyu.space>
2024-08-11 02:34:09 +0200

    Initial commit

---
+++ bahnmod.h
@@ -0,0 +1,111 @@
+#include "icons.h"
+
+void KoyuWatchy::drawBahn(int variant, float batt) {
+
+
+    // ** SETUP **
+
+  int16_t  x1, y1, lasty;
+  uint16_t w, h;
+  String textstring;
+  
+  // ** DRAW **
+
+  //drawbg
+  display.fillScreen((variant == 0) ? GxEPD_WHITE : GxEPD_BLACK);
+  display.fillRoundRect(2, 2, 196, 196, 8, GxEPD_BLACK);
+  display.fillRoundRect(6, 6, 188, 188, 5, (variant == 0) ? GxEPD_WHITE : GxEPD_BLACK);
+
+  display.setFont(&DIN_1451_Engschrift_Regular64pt7b);
+  display.setTextColor((variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  display.setTextWrap(false);
+
+  //draw hours
+  textstring = currentTime.Hour;
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(183 - w, 100 - 5);
+  display.print(textstring);
+
+  //draw minutes
+  if (currentTime.Minute < 10) {
+    textstring = "0";
+  } else {
+    textstring = "";
+  }
+  textstring += currentTime.Minute;
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(183 - w, 100 + 3 + h);
+  display.print(textstring);
+
+  // draw battery
+  #ifdef ARDUINO_ESP32S3_DEV
+  display.fillRoundRect(16, 16, 34, 12, 4, (variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  display.fillRoundRect(49, 20, 3, 4, 2, (variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  display.fillRoundRect(18, 18, 30, 8, 3, (variant == 0) ? GxEPD_WHITE : GxEPD_BLACK);
+  if (batt > 0) {
+    display.fillRoundRect(20, 20, 26 * batt, 4, 2, (variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  }
+  if(USB_PLUGGED_IN){
+    display.drawBitmap(56, 12, charge, 16, 18, (variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  }
+  #endif
+  
+   
+  lasty = 200 - 16;
+
+  //draw weather
+  weatherData currentWeather = getWeatherData();
+  int8_t temperature = currentWeather.temperature;
+  int16_t weatherConditionCode = currentWeather.weatherConditionCode;
+  if (WIFI_CONFIGURED) {
+    textstring = String(temperature)+" °C";
+  } else {
+    temperature = temperature / 1.5;
+    temperature = (int)temperature;
+    textstring = String(temperature) + " °C";
+  }
+  display.setFont(&DIN_1451_Engschrift_Regular12pt7b);
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.fillRoundRect(16, lasty - h - 2, w + 7, h + 4, 2, (variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  display.setCursor(19, lasty - 3);
+  display.setTextColor((variant == 0) ? GxEPD_WHITE : GxEPD_BLACK);
+  display.print(textstring);
+  display.setTextColor((variant == 0) ? GxEPD_BLACK : GxEPD_WHITE);
+  lasty += -8 - h;
+
+  // draw year
+  textstring = currentTime.Year + 1970;
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(16, lasty);
+  display.print(textstring);
+  lasty += -20;
+
+  // draw date
+  textstring = monthShortStr(currentTime.Month);
+  textstring += " ";
+  textstring += currentTime.Day;
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(16, lasty);
+  display.print(textstring);
+  lasty += -20;
+
+  // draw day
+  textstring = dayStr(currentTime.Wday);
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(16, lasty);
+  display.print(textstring);
+  lasty += -40;
+
+  // draw watch name
+  textstring = "Watchy";
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(16, lasty);
+  display.print(textstring);
+  lasty += -20;
+  textstring = "Leonie's";
+  display.getTextBounds(textstring, 0, 0, &x1, &y1, &w, &h);
+  display.setCursor(16, lasty);
+  display.print(textstring);
+  lasty += -20;
+
+}
---
+++ main.h
@@ -0,0 +1,74 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <Watchy.h>
+#include "vis.h"
+#include "vcard.h"
+
+RTC_DATA_ATTR int face = 0;
+RTC_DATA_ATTR int faces_vars[6][2] = {{1,0},{1,0},{1,0},{1,0},{2,0},{1,0}}; //variants, current
+
+class KoyuWatchy : public Watchy {
+  using Watchy::Watchy;
+  public:
+    void drawWatchFace();
+    void drawWrapText(String text);
+    void drawCentered(String text, int y2);
+    void drawWeather();
+    void drawBahn(int variant, float batt);
+    virtual void handleButtonPress();
+};
+
+#include "bahnmod.h"
+
+void KoyuWatchy::handleButtonPress() {
+  if (guiState == WATCHFACE_STATE) {
+    //Up and Down switch watch faces
+    uint64_t wakeupBit = esp_sleep_get_ext1_wakeup_status();
+    if (wakeupBit & UP_BTN_MASK) {
+      face--;
+      if (face < 0 ) { face = 1; }
+      RTC.read(currentTime);
+      showWatchFace(false);
+    }
+    if (wakeupBit & DOWN_BTN_MASK) {
+      face++;
+      if (face > 1 ) { face = 0; }
+      RTC.read(currentTime);
+      showWatchFace(false);
+    }
+    if (wakeupBit & BACK_BTN_MASK) {
+      face = 0;
+      RTC.read(currentTime);
+      showWatchFace(false);
+      return;
+    }
+    if (wakeupBit & MENU_BTN_MASK) {
+      Watchy::handleButtonPress();
+      return;
+    }
+  } else {Watchy::handleButtonPress();}
+}
+
+void KoyuWatchy::drawWatchFace() {
+  // ** UPDATE **
+  //resets step counter at midnight everyday
+  if (currentTime.Hour == 00 && currentTime.Minute == 00) {
+    sensor.resetStepCounter();
+  }
+
+  // ** GET BATTERY **
+  float batt = (getBatteryVoltage()-3.3);
+  if (batt > 1) { batt = 1; } else if (batt < 0) { batt = 0; }
+  
+  if (face == 0) {
+    // ** DRAW WATCHFACE **
+    drawBahn(faces_vars[face][1], batt);
+  }
+  if (face == 1) {
+    display.fillScreen(GxEPD_WHITE);
+    display.drawBitmap(0, 0, vcard, 200, 200, GxEPD_BLACK);
+  }
+}
+
+#endif
